8fcec60dcdb358a2384f203ad32ff797dacea182
erdemcasa <pl23ksc@eduvaud.ch>
2026-09-14 10:11:42 +0200

    feat(parser): Ajout du Parser pour les températures.

---
+++ FUN/Paser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using FUN;
+
+namespace DataSeries;
+
+public class Parser
+{
+    public static DataPoint<Weather> ParseWeather(string[] cols)
+    {
+        if (cols == null || cols.Length < 3)
+        {
+            throw new ArgumentException("La ligne doit contenir au moins 3 colonnes (Date, Ville, Température).", nameof(cols));
+        }   
+
+        DateTime date = DateTime.Parse(cols[0], CultureInfo.InvariantCulture);
+        string cityName = cols[1];
+        double temperature = double.Parse(cols[2], CultureInfo.InvariantCulture);
+        double degres = cols.Length > 3
+            ? double.Parse(cols[3], CultureInfo.InvariantCulture)
+            : 0;
+
+        Weather weather = new(date, cityName, temperature, degres);
+        return new DataPoint<Weather>(date, weather);
+    }
+}
