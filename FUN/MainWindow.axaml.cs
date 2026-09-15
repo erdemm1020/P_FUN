f952960375398839fa8cdccf67e92f49ae001a10
erdemcasa <pl23ksc@eduvaud.ch>
2026-09-15 19:48:22 +0200

    feta(plot): Possibilité d'afficher les courbes qu'on veut avec un checkbox

--- FUN/MainWindow.axaml.cs
+++ FUN/MainWindow.axaml.cs
@@ -18,34 +18,39 @@ public partial class MainWindow : Window
         DataSeries<DataPoint<Weather>> series = DataSeries<DataPoint<Weather>>.FromCsv("weather_data.csv", Parser.ParseWeather);
         Console.WriteLine($"Nombre d'éléments : {series.Count()}");
         
-        var city = series.Select(dp => dp.Value.CityName).Distinct();
-        Console.WriteLine($"Villes dans le csv : {string.Join(", ", city)}");
-
-        var checkBoxes = city.Select(cityName => new CheckBox
-        {
-            Content = cityName,
-            IsChecked = true,
-            Foreground = Brush.Parse("#cdd6f4"),
-            Margin = new Thickness(12, 0)
-        });
-
-        CityCheckBoxContainer.Children.AddRange(checkBoxes);
-        
-        series.GroupBy(dp => dp.Value.CityName)
-            .ToList()
-            .ForEach(grupByCities => 
+        var checkBoxes = series.GroupBy(dp => dp.Value.CityName)
+            .Select(grupByCities => 
             {
                 double[] dates = grupByCities.Select(dp => dp.Timestamp.ToOADate()).ToArray();
                 double[] temps = grupByCities.Select(dp => dp.Value.Temperature).ToArray();
                   
                 var scatter = MyPlot.Plot.Add.Scatter(dates, temps);
                 scatter.Label = grupByCities.Key;
+
+                var checkBox = new CheckBox
+                {
+                    Content = grupByCities.Key,
+                    IsChecked = true,
+                    Foreground = Brush.Parse("#cdd6f4"),
+                    Margin = new Thickness(12, 0)
+                };
+
+                checkBox.IsCheckedChanged += (sender, args) => 
+                {
+                    scatter.IsVisible = checkBox.IsChecked == true;
+    
+                    MyPlot.Refresh();
+                };
+
+                return checkBox;
             });
+
+        CityCheckBoxContainer.Children.AddRange(checkBoxes);
+
+        MyPlot.IsHitTestVisible = false; 
         
         MyPlot.Plot.Axes.DateTimeTicksBottom();
-        
         MyPlot.Plot.ShowLegend(); 
-        
         MyPlot.Refresh();
     }
 }
